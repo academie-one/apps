1b44b509012e8a8df2be09f8e778c7585d391fc8
asansyzb <asansyzb@student.42.us.org>
2020-12-10 15:14:13 +0600

    json fixed

---
+++ client/components/Methodology/Main.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Main = (data) => {
+    return (
+        <>
+            <h1>{data.title}</h1>
+            <p>{data.body}</p>
+        </>
+    );
+};
+
+export default Main;
--- client/pages/methodology.js
+++ client/pages/methodology.js
@@ -1,5 +1,15 @@
+import * as React from 'react';
+import {Methodology as data} from '../data/Content.json';
+import Main from '../components/Methodology/Main';
+
 const Methodology = () => {
-  return <h1>Methodology</h1>;
+  return (
+    <>
+    {console.log(data)}
+    {/* Hi */}
+      <Main data={data.Main}/>
+    </>
+  );
 };
 
 export default Methodology;
