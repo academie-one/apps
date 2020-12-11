eb3bb38c662771ba44d4957688aa1ce28c370aa7
asansyzb <asansyzb@student.42.us.org>
2020-12-11 12:06:02 +0600

    fixed sidebar

--- client/components/shared/Sidebar.js
+++ client/components/shared/Sidebar.js
@@ -9,36 +9,34 @@ const Sidebar = ({classes}) => {
       <nav className={`${styles.nav} ${styles.pulsate}`}>
         <ul className="navbar--link">
           <li className={styles.navbarLinkItem}>
-            <Link href="/">
-              <Typography variant="nav">academie one</Typography>
-            </Link>
+            <Typography variant="nav">
+              <Link href="/">academie one</Link>
+            </Typography>
           </li>
           <li className={styles.navbarLinkItem}>
-            <Link href="/foundation">
-              <Typography variant="nav">foundation</Typography>
-            </Link>
+            <Typography variant="nav">
+              <Link href="/foundation">foundation</Link>
+            </Typography>
           </li>
           <li className={styles.navbarLinkItem}>
-            <Link href="/academie">
-              <Typography variant="nav">academie</Typography>
-            </Link>
+            <Typography variant="nav">
+              <Link href="/academie">academie</Link>
+            </Typography>
           </li>
           <li className={styles.navbarLinkItem}>
-            <Link href="/methodology">
-              <Typography variant="nav">methodology</Typography>
-            </Link>
+            <Typography variant="nav">
+              <Link href="/methodology">methodology</Link>
+            </Typography>
           </li>
           <li className={styles.navbarLinkItem}>
-            <Link href="/admissions">
-              <Typography variant="nav">admissions</Typography>
-            </Link>
+            <Typography variant="nav">
+              <Link href="/admissions">admissions</Link>
+            </Typography>
           </li>
           <li className={styles.navbarLinkItem}>
-            <Link href="/coming-soon">
-              <Typography variant="nav" color="blue">
-                apply
-              </Typography>
-            </Link>
+            <Typography variant="nav" color="blue">
+              <Link href="/coming-soon">apply</Link>
+            </Typography>
           </li>
         </ul>
       </nav>
