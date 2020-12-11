64d7baff24a5a2345b1b83be6b810b19ecd432ce
asansyzb <asansyzb@student.42.us.org>
2020-12-11 11:43:34 +0600

    fata

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,15 +3,15 @@ import Typography from '../components/shared/Typography';
 const Home = () => {
   return (
     <>
-      <Typography variant="h1">Can the future be changed? </Typography>
+      <Typography variant="h1">Can the future be changed?</Typography>
 
-      <Typography variant="h2">Can the future be changed? </Typography>
+      <Typography variant="h2">Can the future be changed?</Typography>
 
-      <Typography variant="h3">Can the future be changed? </Typography>
+      <Typography variant="h3">Can the future be changed?</Typography>
 
-      <Typography variant="h4">Can the future be changed? </Typography>
+      <Typography variant="h4">Can the future be changed?</Typography>
 
-      <Typography variant="body">Can the future be changed? </Typography>
+      <Typography variant="body">Can the future be changed?</Typography>
 
       <Typography variant="h1">Be One.</Typography>
     </>
