0feaea4e76f02c8ce55972de0cde0fb498631cd9
asansyzb <asansyzb@student.42.us.org>
2020-12-14 15:32:09 +0600

    Apply now triggers glowing

--- client/components/shared/Sidebar.js
+++ client/components/shared/Sidebar.js
@@ -6,13 +6,24 @@ import Link from 'next/link';
 // TODO: Active links bold
 // TODO: Apply triggers gloving
 const Sidebar = ({ classes }) => {
+  const [glowing, setGlowing] = React.useState(false);
+
+  const applyHovered = {
+    boxShadow: '0px 0px 300px rgba(195, 218, 231, 1)',
+    transition: 'box-shadow 0.5s ease-in-out'
+
+  };
+  const applyUnhovered = {
+    transition: 'box-shadow 0.5s ease-in-out'
+
+  };
   return (
     <aside className={classes}>
-      <nav className={`${styles.nav}`} />
+      <nav style={glowing ? applyHovered : applyUnhovered} className={`${styles.nav}`} />
       <ul className={`${styles.navUlLinks}`} >
         <li className={styles.navbarLinkItem}>
           <Typography variant="menu">
-            <Link href="/foundation">foundation</Link>
+            <Link activeClassName="active" href="/foundation" onlyActiveOnIndex>foundation</Link>
           </Typography>
         </li>
         <li className={styles.navbarLinkItem}>
@@ -30,7 +41,11 @@ const Sidebar = ({ classes }) => {
             <Link href="/admissions">admissions</Link>
           </Typography>
         </li>
-        <li className={styles.navbarLinkItem}>
+        <li
+          onMouseLeave={() => setGlowing(false)}
+          onMouseOver={() => setGlowing(true)}
+
+          className={styles.navbarLinkItem}>
           <Typography variant="menu" color="blue">
             <Link href="/coming-soon">apply</Link>
           </Typography>
