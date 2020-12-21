d448b2ff8e4480e106453c45f7d2392efa3998d4
asansyzb <asansyzb@student.42.us.org>
2020-12-21 12:03:14 +0600

    router highligh and opacity possible fix

--- client/components/shared/Header.js
+++ client/components/shared/Header.js
@@ -2,11 +2,18 @@ import React, {useState} from 'react';
 import Link from 'next/link';
 import Typography from '../shared/Typography';
 import Footer from '../shared/Footer';
+import {useRouter} from 'next/router';
 
 import styles from '../../styles/components/shared/Header.module.css';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const currentPage = router.route;
+
+  const highlightCurrentPage = {
+    textDecoration: 'underline',
+  };
 
   const handleClick = () => {
     setOpen(!open);
@@ -14,7 +21,9 @@ const Header = () => {
 
   return (
     <header className={styles.header}>
-      <nav className={`${styles.nav} ${open ? `` : `bg-opacity-95`}`}>
+      <nav
+        className={`${styles.nav} ${open ? `bg-opacity-100` : `bg-opacity-90`}`}
+      >
         <div className={styles.navContent}>
           <Link href="/" passHref>
             <button className={`w-40 h-10`}>
@@ -37,27 +46,43 @@ const Header = () => {
         </div>
         <ul className={`${styles.menu} ${open ? 'flex' : 'hidden'}`}>
           <li className={styles.navbarLinkItem} onClick={handleClick}>
-            <Typography variant="menu">
+            <Typography
+              variant="menu"
+              style={currentPage === '/' ? highlightCurrentPage : {}}
+            >
               <Link href="/">Foundation</Link>
             </Typography>
           </li>
           <li className={styles.navbarLinkItem} onClick={handleClick}>
-            <Typography variant="menu">
+            <Typography
+              variant="menu"
+              style={currentPage === '/academie' ? highlightCurrentPage : {}}
+            >
               <Link href="/academie">Academie</Link>
             </Typography>
           </li>
           <li className={styles.navbarLinkItem} onClick={handleClick}>
-            <Typography variant="menu">
+            <Typography
+              variant="menu"
+              style={currentPage === '/methodology' ? highlightCurrentPage : {}}
+            >
               <Link href="/methodology">Methodology</Link>
             </Typography>
           </li>
           <li className={styles.navbarLinkItem} onClick={handleClick}>
-            <Typography variant="menu">
+            <Typography
+              variant="menu"
+              style={currentPage === '/admissions' ? highlightCurrentPage : {}}
+            >
               <Link href="/admissions">Admission</Link>
             </Typography>
           </li>
           <li className={styles.navbarLinkItem} onClick={handleClick}>
-            <Typography variant="menu" className={`text-blueOne`}>
+            <Typography
+              variant="menu"
+              className={`text-blueOne`}
+              style={currentPage === '/coming-soon' ? highlightCurrentPage : {}}
+            >
               <Link href="/coming-soon">Apply</Link>
             </Typography>
           </li>
