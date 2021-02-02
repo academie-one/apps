33d8136541a5f93b7e1db57fced3bf5e34d3c1b9
asansyzb <asansyzb@student.42.us.org>
2021-02-02 15:37:36 +0600

    layout changes and home hero changes for mobile

--- client/components/Home/HomeHero.js
+++ client/components/Home/HomeHero.js
@@ -1,11 +1,19 @@
 import Typography from '../shared/Typography';
 import styles from '../../styles/components/Home/HomeHero.module.css';
+import Image from 'next/image';
 
 const HomeHero = ({data}) => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <img src={data.img} alt="main" className={styles.image} />
+        <Image
+          src={data.img}
+          alt="academie logo"
+          className={styles.image}
+          width={150}
+          height={150}
+          layout="responsive"
+        />
       </div>
       <div className={styles.content}>
         <div>
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,15 @@
 import {
   Academie as data,
-  Admissions as admissionData,
+  // Admissions as admissionData,
 } from '../data/Content.json';
 import HomeHero from '../components/Home/HomeHero';
-import ImportantDates from '../components/Admissions/ImportantDates';
+//import ImportantDates from '../components/Admissions/ImportantDates';
 
 const Home = () => {
   return (
     <>
       <HomeHero data={data.Hero} />
-      { /* <ImportantDates data={admissionData.ImportantDates} /> */}
+      {/* <ImportantDates data={admissionData.ImportantDates} /> */}
     </>
   );
 };
