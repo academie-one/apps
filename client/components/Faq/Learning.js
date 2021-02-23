02a389d39e478d207483593d4526ff25b9ed23fb
hhaakkimm <hhaakkimm@gmail.com>
2021-02-24 01:18:18 +0600

    some tests

--- client/components/Faq/Learning.js
+++ client/components/Faq/Learning.js
@@ -1,20 +1,40 @@
+import * as React from 'react';
 import Typography from '../shared/Typography';
 import styles from '../../styles/components/Faq/Learning.module.css';
 
 const Learning = ({data}) => {
+  const [clicked, setClicked] = React.useState(false);
+  
+  const handleClick = () => {
+    setClicked(!clicked)
+  }
+
   return (
     <>
       <div className={styles.items}>
-        {data.items.map((el, index) => (
-          <div key={index} className={styles.module}>
-            <Typography className={styles.question} variant="h4">
-              {el.question}
-            </Typography>
-            <Typography className={styles.answer} variant="body">
-              {el.answer}
-            </Typography>
+        {data.sections.map((en, index) => (
+          <div key={index} onClick={handleClick}>
+            <Typography variant="h2"
+            >{en.h2Title}</Typography>
+            {clicked && (
+              <>
+                {en.items.map((el, index2) => (
+                  <div key={index2} className={styles.module}>
+                    <Typography className={styles.question} variant="h4">
+                      {el.question}
+                    </Typography>
+                    <Typography className={styles.answer} variant="body">
+                      {el.answer}
+                    </Typography>
+                  </div>
+                ))}
+              </>
+            )}
+              
+            
+            {console.log(clicked)}
           </div>
-        ))}
+         ))} 
       </div>
     </>
   );
