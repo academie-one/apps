c5132a31d6d5f661b591c3c4e327528f331e5861
hhaakkimm <hhaakkimm@gmail.com>
2021-02-26 14:56:52 +0600

    faq page updated

---
+++ client/components/Faq/Practical.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Typography from '../shared/Typography';
+import styles from '../../styles/components/Faq/Learning.module.css';
+
+const Practical = ({data}) => {
+  const [clicked, setClicked] = React.useState(false);
+  const [drop, setDrop] = React.useState('+');
+
+  const handleClick = () => {
+    setClicked(!clicked)
+    drop == '+' ? setDrop('-') : setDrop('+');
+  }
+
+  return (
+    <>
+      <div className={styles.items}>
+            <div className={styles.clickable} onClick={handleClick}>
+              <Typography variant="h3">{data.h2Title}</Typography>
+              <Typography variant="h3">
+                {drop}
+              </Typography>
+            </div>
+            {clicked && (
+              <>
+                {data.items.map((el, index2) => (
+                  <div key={index2} className={styles.module}>
+                    <Typography className={styles.question} variant="h4">
+                      {el.question}
+                    </Typography>
+                    <Typography className={styles.answer} variant="body">
+                      {el.answer}
+                    </Typography>
+                  </div>
+                ))}
+              </>
+            )}
+            <hr className={styles.hr}/>
+          </div>
+    </>
+  );
+};
+
+export default Practical;
--- client/pages/faq.js
+++ client/pages/faq.js
@@ -2,6 +2,7 @@ import Curriculum from '../components/Faq/Curriculum';
 import Graduation from '../components/Faq/Graduation';
 import Learning from '../components/Faq/Learning';
 import PiscineFaq from '../components/Faq/Piscinefaq';
+import Practical from '../components/Faq/Practical';
 import Requirements from '../components/Faq/Requirements';
 import Container from '../components/shared/Container';
 import {Faq as faqData} from '../data/Content.json';
@@ -15,6 +16,7 @@ const Faq = () => {
       <PiscineFaq data={faqData.PiscineFaq} />
       <Curriculum data={faqData.Curriculum} />
       <Graduation data={faqData.Graduation} />
+      <Practical data={faqData.Practical} />
     </>
   );
 };
