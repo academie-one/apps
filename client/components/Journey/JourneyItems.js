e1901b06ee7f7c261a6a30db5343a10cfe24a7d8
asansyzb <asansyzb@student.42.us.org>
2021-04-29 22:44:18 +0600

    journey containers

---
+++ client/components/Journey/JourneyItems.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from '../../styles/components/Journey/JourneyItems.module.css';
+import Typography from '../shared/Typography';
+
+const JourneyItems = ({items, direction = 'row'}) => {
+  return (
+    <div className={direction === 'row' ? styles.itemsRow : styles.itemsCol}>
+      {items && items.map((el, index) => (
+        <div key={index} className={styles.item}>
+          {el.img && (
+            <img className={`w-full h-60`} src={el.img} alt={el.imgAlt} />
+          )}
+
+          {el.title && (
+            <Typography variant={el.titleVariant} className={el.titleClass}>
+              {el.title}
+            </Typography>
+          )}
+          {el.body && (
+            <Typography variant={el.bodyVariant} className={el.bodyClass}>
+              {el.body}
+            </Typography>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default JourneyItems;
--- client/pages/journey.js
+++ client/pages/journey.js
@@ -1,22 +1,21 @@
 import * as React from 'react';
 import {Journey as data} from '../data/Content.json';
-import Piscine from '../components/Methodology/Piscine';
-import Skills from '../components/Methodology/Skills';
 import Spec from '../components/Methodology/Spec';
 
 import Container from '../components/shared/Container';
+import JourneyContainer from '../components/Journey/JourneyContainer';
 
 const Methodology = () => {
   return (
     <>
       <Container data={data.PageName} />
       <Container data={data.Journey} />
-      <Piscine data={data.Piscine} />
-      <Container data={data.Cadet} />
-      <Skills data={data.Skills} />
+      <JourneyContainer data={data.Piscine} />
+      <JourneyContainer data={data.Cadet} />
       <Spec data={data.Spec} />
+      <JourneyContainer data={data.Career} itemsDirection={'col'}/>
+
       <Container data={data.Portfolio} />
-      <Container data={data.Career} />
       <Container data={data.Contract} />
       <Container data={data.Offer} />
       <Container data={data.Alumni} />
