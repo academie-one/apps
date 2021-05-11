85e67c3634a98d3613a279adf4ff95ef94d28814
asansyzb <asansyzb@student.42.us.org>
2021-05-11 13:05:21 +0600

    academie reworked

--- client/pages/academie.js
+++ client/pages/academie.js
@@ -1,15 +1,16 @@
 import {Academie as data} from '../data/Content.json';
 import Container from '../components/shared/Container';
+import JourneyContainer from "../components/Journey/JourneyContainer";
 import Graduates from '../components/Academie/Graduates';
 
 const Academie = () => {
   return (
     <>
-      <Container data={data.PageName} />
-      <Container data={data.Concept} />
-      <Container data={data.Edu} />
+      <JourneyContainer data={data.PageName} />
+      <JourneyContainer data={data.Concept} itemsDirection={'col'} />
+      <JourneyContainer data={data.Edu} />
       <Graduates data={data.Graduates} />
-      <Container data={data.Methodology} />
+      <JourneyContainer data={data.Methodology} itemsDirection={'col'} />
       <Container data={data.Campus} />
       <Container data={data.Almaty} />
     </>
