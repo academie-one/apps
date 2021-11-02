b031e7bbe6a9526dfb5d3e6da419dfc1935fc1f4
hhaakkimm <hhaakkimm@gmail.com>
2021-11-03 05:22:40 +0600

    almost finished

--- terms/components/Home/HomeHero.js
+++ terms/components/Home/HomeHero.js
@@ -4,6 +4,18 @@ const HomeHero = ( {data} ) => {
   return (
     <div>
       <Container data={data.General} />
+      <Container data={data.Definitions} />
+      <Container data={data.Subject} />
+      <Container data={data.Obligation} />
+      <Container data={data.Responsibility} />
+      <Container data={data.Conditions} />
+      <Container data={data.Usage} />
+      <Container data={data.CollectionData} />
+      <Container data={data.Goals} />
+      <Container data={data.PersonalData} />
+      <Container data={data.Crossborder} />
+      <Container data={data.Note} />
+      <Container data={data.Final} />
     </div>
   );
 };
--- terms/pages/index.js
+++ terms/pages/index.js
@@ -1,4 +1,5 @@
 import HomeHero from '../components/Home/HomeHero';
+import * as React from 'react';
 import {Terms as data} from '../data/Content.json';
 
 const Home = () => {
