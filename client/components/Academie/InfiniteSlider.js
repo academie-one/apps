68c33fa303bbb20557ee425e4328b62e50ce7d57
Iskander Iskakov <tradeshells@gmail.com>
2021-11-03 09:55:48 +0600

    Added map; Logos are changing color to white.

--- client/components/Academie/InfiniteSlider.js
+++ client/components/Academie/InfiniteSlider.js
@@ -1,14 +1,15 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import styles from '../../styles/components/Academie/InfiniteSlider.module.css';
 import ThemeContext from '../shared/ThemeContext';
 
-const Carousel = ({items}) => {
-  const {theme} = useContext(ThemeContext);
+const Carousel = ({ items }) => {
+  const { theme } = useContext(ThemeContext);
+  const isDark = localStorage.getItem('theme')
+
   return (
     <div
-      className={`${styles.slider} ${
-        theme === 'dark' ? styles.sliderDark : styles.sliderWhite
-      }`}
+      className={`${styles.slider} ${theme === 'dark' ? styles.sliderDark : styles.sliderWhite
+        }`}
     >
       <div className={styles.slideTrack}>
         {[0, 1].map((it) =>
@@ -18,6 +19,7 @@ const Carousel = ({items}) => {
               src={item.img}
               className={styles.companyLogo}
               alt={item.imgAlt}
+              id={localStorage.getItem('theme') === 'dark' ? styles.whiteLogo : null}
             />
           )),
         )}
