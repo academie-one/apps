0bc2aaa037addd24b3727e90bba947a04a730c2e
hhaakkimm <hhaakkimm@gmail.com>
2021-11-03 15:35:52 +0600

    hero page fixed & updated

--- client/components/Home/HomeHero.js
+++ client/components/Home/HomeHero.js
@@ -2,7 +2,12 @@ import React, {useContext} from 'react';
 import Typography from '../shared/Typography';
 import styles from '../../styles/components/Home/HomeHero.module.css';
 import ThemeContext from '../shared/ThemeContext';
-import { isMobile } from 'react-device-detect';
+
+let flag = 0;
+
+const chooseImg = (data) => {
+  return (flag ? data.heroImage : data.img);
+}
 
 const HomeHero = ({data}) => {
   const {theme} = useContext(ThemeContext);
@@ -12,14 +17,19 @@ const HomeHero = ({data}) => {
     filter: 'invert(1)',
   };
 
+  React.useEffect(() => {
+    if (window.innerWidth >= 768)
+      flag = 1;
+  }, []);
+
+
   return (
     <div className={styles.container}>
       <div className={styles.leftText}>
         <Typography variant={'h2'}>{"Code your\n future"}</Typography>
         <Typography variant={'h4'}>{"Start IT career"}</Typography>
       </div>
-      {console.log(isMobile)}
-      <img src={(isMobile) ? data.img : data.heroImage} alt="mobile-wtf"/>
+      <img src={chooseImg(data)} alt="mobile-wtf"/>
     </div>
   );
 };
