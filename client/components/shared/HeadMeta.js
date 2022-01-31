b9a6e1dc7d03529fc44a1db99f08b15afdcd3ce6
hhaakkimm <hhaakkimm@gmail.com>
2022-01-31 12:48:38 +0600

    meta updated

--- client/components/shared/HeadMeta.js
+++ client/components/shared/HeadMeta.js
@@ -9,20 +9,20 @@ const HeadMeta = ({title}) => {
       
       <title>Academie One: education and guaranteed employment in IT</title>
       <meta name="title" content="Academie One: education and guaranteed employment in IT" />
-      <meta name="description" content="Welcome to Academie One - the modern training system for programming and career development in IT. We are welcoming anyone from 18 to 42. Everyone can change the future – be One." />
+      <meta name="description" content="The modern training system for programming and career development in IT. We are welcoming anyone from 18 to 42. Everyone can change the future – be One." />
 
       
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://www.academie.one/" />
       <meta property="og:title" content="Academie One: education and guaranteed employment in IT" />
-      <meta property="og:description" content="Welcome to Academie One - the modern training system for programming and career development in IT. We are welcoming anyone from 18 to 42. Everyone can change the future – be One." />
+      <meta property="og:description" content="The modern training system for programming and career development in IT. We are welcoming anyone from 18 to 42. Everyone can change the future – be One." />
       <meta property="og:image" content="" />
 
       
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://www.academie.one/" />
       <meta property="twitter:title" content="Academie One: education and guaranteed employment in IT" />
-      <meta property="twitter:description" content="Welcome to Academie One - the modern training system for programming and career development in IT. We are welcoming anyone from 18 to 42. Everyone can change the future – be One." />
+      <meta property="twitter:description" content="The modern training system for programming and career development in IT. We are welcoming anyone from 18 to 42. Everyone can change the future – be One." />
       <meta property="twitter:image" content=""></meta>
       <link rel="icon" href="/icons/circle.svg" />
     </Head>
