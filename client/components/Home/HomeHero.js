b174a65370f9c478ac83e1f9aa5b875495f12369
hhaakkimm <hhaakkimm@gmail.com>
2022-04-05 17:18:19 +0600

    saddd

--- client/components/Home/HomeHero.js
+++ client/components/Home/HomeHero.js
@@ -1,12 +1,15 @@
-import React, {useContext} from 'react';
+import React, {useContext } from 'react';
 import Typography from '../shared/Typography';
 import styles from '../../styles/components/Home/HomeHero.module.css';
 import ThemeContext from '../shared/ThemeContext';
+import YouTube from 'react-youtube';
 
 let flag = 0;
 
-const chooseImg = (theme, data) => {
-  return (theme === 'dark' ? data.video2 : data.video1);
+const chooseImg = (theme, data, isMobile) => {
+  if (!isMobile)
+    return (theme === 'dark' ?  data.video1 : data.video2);
+  return (theme === 'dark' ?  data.mob_video1 : data.mob_video2);
 }
 
 const HomeHero = ({data}) => {
@@ -23,7 +26,6 @@ const HomeHero = ({data}) => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
   
-
   const {theme} = useContext(ThemeContext);
 
   const inverted = {
@@ -31,12 +33,37 @@ const HomeHero = ({data}) => {
     filter: 'invert(1)',
   };
 
+  const opts = {
+    height: 'h-screen',
+    width: 'w-screen',
+    playerVars: {
+      controls: 0,
+      autoPlay: 1, 
+      loop: 1
+    },
+  };
+
 
   return (
     <div className={styles.container}>
-        <video className={styles.backVideo} autoPlay muted loop>
-          <source src={data.video1}  type="video/mp4" />
+      {theme == 'dark' ? (
+        <video key={1} className={styles.backVideo} autoPlay muted>
+          {isMobile ?
+            <source src={data.mob_video2} type="video/mp4" />
+            :
+            <source src={data.video2} type="video/mp4" />
+          }
         </video>
+        ) : (
+          <video key={2} className={styles.backVideo} autoPlay muted>
+            {isMobile ?
+            <source src={data.mob_video1} type="video/mp4" />
+            :
+            <source src={data.video1} type="video/mp4" />
+          }
+          </video>
+        )}
+        {/* <YouTube className={styles.backVideo} videoId={chooseImg(theme)} opts={opts}/> */}
     </div>
   );
 };
